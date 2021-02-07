58449b3e98202d11ee8c303787c82954baa855c5
taavettik <taavetti.kukkonen@taitounited.fi>
2021-02-07 18:31:43 +0200

    debug: db connection info

--- server/src/common/db.ts
+++ server/src/common/db.ts
@@ -2,6 +2,10 @@ import pgp from 'pg-promise';
 import { config } from './config';
 
 export function createDb() {
+  console.log(
+    `Connecting to ${config.DB_USER}@${config.DB_HOST}/${config.DB_NAME}`,
+  );
+
   const pg = pgp();
   const db = pg({
     host: config.DB_HOST,
