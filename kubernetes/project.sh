8fb1c78e2f9a4b5f87bd4dad33bc119954e76945
taavettik <taavetti.kukkonen@taitounited.fi>
2021-02-07 11:41:17 +0200

    ci: client deployment

--- kubernetes/project.sh
+++ kubernetes/project.sh
@@ -1,7 +1,9 @@
 export PROJECT_NAME=riffy
-export HOST=riffy-dev.kukkonen.dev
 
 if [ $PROJECT_ENV = 'prod' ]
 then
   export HOST=riffy.kukkonen.dev
+elif [ $PROJECT_ENV = 'dev' ]
+then
+  export HOST=riffy-dev.kukkonen.dev
 fi
